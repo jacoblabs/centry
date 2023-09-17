af6cd01014c325b22bbc4cbb354f8b8ee4c56c9e
billy <huthealth@naver.com>
2023-09-17 11:59:58 +0900

    add home page mouse over event

--- centry-frontend/src/HomePage.tsx
+++ centry-frontend/src/HomePage.tsx
@@ -8,23 +8,30 @@ import { Component, useCallback } from 'react'
 import Nav from 'react-bootstrap/Nav'
 import { ListGroup } from 'react-bootstrap'
 import dayjs from 'dayjs'
+import { useNavigate } from 'react-router-dom'
 
 interface Props {
   documents?: Document[]
 }
 
 export default function (props: Props) {
+  const navigate = useNavigate()
+
   const formatDate = (timestamp: number) => {
     const dayDiff = dayjs().diff(timestamp, 'day')
     if (dayDiff > 0) {
       return `${dayDiff}일 전`
     }
-    
+
     const hourDiff = dayjs().diff(timestamp, 'hour')
-    if(hourDiff > 0) {
+    if (hourDiff > 0) {
       return `${hourDiff}시간 전`
     }
     return '조금전'
+  }
+
+  const handleDocumentClick = (doc: Document) => {
+    navigate(`/${doc.id}`)
   }
 
   return (
@@ -32,14 +39,16 @@ export default function (props: Props) {
       <ListGroup as="ol" className="mt-3 mb-5">
         {props.documents?.map((document) => (
           <ListGroup.Item
+            action
             as="li"
             className="d-flex justify-content-between align-items-start"
+            style={{cursor: 'pointer'}}
+            onClick={() => handleDocumentClick(document)}
           >
             <div className="ms-2 me-auto d-flex flex-column">
               <div className="d-flex">
                 <div className="fw-bold flex-grow-1">{document.title}</div>
-                <div>{formatDate(document.createdAt ?? 0)}
-                </div>
+                <div>{formatDate(document.createdAt ?? 0)}</div>
               </div>
               <div
                 style={{
