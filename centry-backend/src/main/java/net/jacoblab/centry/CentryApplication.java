8123443fc1a06b91000cf77da3098e0427d71e5f
billy <huthealth@naver.com>
2023-10-08 10:45:02 +0900

    backend init

---
+++ centry-backend/src/main/java/net/jacoblab/centry/CentryApplication.java
@@ -0,0 +1,13 @@
+package net.jacoblab.centry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentryApplication.class, args);
+	}
+
+}
---
+++ centry-backend/src/test/java/net/jacoblab/centry/CentryApplicationTests.java
@@ -0,0 +1,13 @@
+package net.jacoblab.centry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
